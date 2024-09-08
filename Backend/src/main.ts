cbda394e917a8aaad1ec9472a27c5ad21a460531
eduardoe92 <eduardo.elias.1192@gmail.com>
2024-09-08 16:52:32 -0300

    main.ts

--- Backend/src/main.ts
+++ Backend/src/main.ts
@@ -5,7 +5,12 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const port = process.env.PORT || 3000;
 
-  app.enableCors();
+  app.enableCors({
+    origin: "https://ruleta-app.vercel.app",
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+    credentials: true,
+  });
+
   await app.listen(port);
   console.log(`Servidor ejecutándose en http://localhost:${port}`);
 }
